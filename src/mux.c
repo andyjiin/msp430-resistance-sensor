2053d8ad28f1587538d20a961ed6f4759bca81c7
andyjiin <34169386+andyjiin@users.noreply.github.com>
2019-05-31 11:25:06 -0400

    Fix mux select lines

--- src/mux.c
+++ src/mux.c
@@ -23,12 +23,12 @@ void mux_select(mux_ref_resistor resistor) {
         case MUX_REF_RESISTOR_100_OHM:
             GPIO_setOutputLowOnPin(GPIO_PORT_MUX_SELECT0, GPIO_PIN_MUX_SELECT0);
             GPIO_setOutputLowOnPin(GPIO_PORT_MUX_SELECT1, GPIO_PIN_MUX_SELECT1);
-            GPIO_setOutputHighOnPin(GPIO_PORT_MUX_SELECT2, GPIO_PIN_MUX_SELECT2);
+            GPIO_setOutputLowOnPin(GPIO_PORT_MUX_SELECT2, GPIO_PIN_MUX_SELECT2);
             break;
         case MUX_REF_RESISTOR_1_KOHM:
             GPIO_setOutputLowOnPin(GPIO_PORT_MUX_SELECT0, GPIO_PIN_MUX_SELECT0);
-            GPIO_setOutputHighOnPin(GPIO_PORT_MUX_SELECT1, GPIO_PIN_MUX_SELECT1);
-            GPIO_setOutputLowOnPin(GPIO_PORT_MUX_SELECT2, GPIO_PIN_MUX_SELECT2);
+            GPIO_setOutputLowOnPin(GPIO_PORT_MUX_SELECT1, GPIO_PIN_MUX_SELECT1);
+            GPIO_setOutputHighOnPin(GPIO_PORT_MUX_SELECT2, GPIO_PIN_MUX_SELECT2);
             break;
         case MUX_REF_RESISTOR_1_MOHM:
             break;
